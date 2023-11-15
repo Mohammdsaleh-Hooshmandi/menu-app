bee4c6219a6b5076e705433c48e29e3b0fd37387
Mohammdsaleh Hooshmandi <Mohammdsale.Hooshmandi1382@gmail.com>
2023-11-15 10:23:49 +0330

    show total amounts in header

--- src/components/Header/HeaderOrderBtn.jsx
+++ src/components/Header/HeaderOrderBtn.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import ListContext from "../../store/list-context";
 
-const HeaderOrderBtn = styled.button`
+const Wrapper = styled.button`
+    position: relative;
     font-size: 1.6rem;
     letter-spacing: .1rem;
     font-weight: 600;
@@ -10,6 +13,32 @@ const HeaderOrderBtn = styled.button`
     cursor: pointer;
     background-color: rgb(232, 126, 75);
     color: #fff;
+
+    .label {
+        width: 2rem;
+        height: 2rem;
+        font-size: 1.5rem;
+        border-radius: 50%;
+        background-color: #790e0e;
+        position: absolute;
+        top: -.5rem;
+        right: -.5rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 `;
+
+const HeaderOrderBtn = props => {
+    const { foods } = useContext(ListContext);
+    const c = foods.map(food => food.amount).reduce((curr, acc) => curr + acc, 0);
+
+    return (
+        <Wrapper onClick={props.onClick}>
+            <div className='label'>{c}</div>
+            {props.children}
+        </Wrapper>
+    );
+};
 
 export default HeaderOrderBtn;
